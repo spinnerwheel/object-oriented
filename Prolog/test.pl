0e43e626ee4f98e9860000f5da6868a53d39fbb0
spinnerwheel <t.rocca3@campus.unimib.it>
2023-12-28 17:27:29 +0100

    Added inheritance's tests

--- Prolog/test.pl
+++ Prolog/test.pl
@@ -18,27 +18,38 @@ test([Test | Rest]) :-
     test(Rest).
 
 test :-
-    Test_sub_this = [
-        method(talk, [], (field(this, name, Value), writeln(Value))) =.. [_,_,_, Body],
-        rewrite_method(tommi, Body, RBody)
-    ],
     Tests = [
         def_class(person, [],
-                  [field(name, 1, integer),
-                   field(university, undefined),
-                   method(talk, [],
+                  [field(name, "Tommi", string),
+                   field(age, 0, integer),
+                   method(talk, [Text],
                           (write("Hi I'm "),
                            field(this, name, Value),
-                           writeln(Value)))]),
-        make(tommi, person, [name = 'Tommi', university = 'Bicocca']),
-        field(tommi, name, 'Tommi'),
-        def_class(animal, [],
-                  [field(specie, undefined),
-                   method(sound, [Arg],
-                          (writeln("Hi I'm a animal!"),
-                           writeln(Arg)))
-                  ]),
-        talk(tommi)
+                           writeln(Value),
+			   write("I have "),
+                           field(this, age, Age),
+                           writeln(Age),
+                            write("I also want to say "),
+                            writeln(Text)))]),
+	make(tommi, person, [name = "Tommi", age = 22]),
+	field(tommi, name, "Tommi"),
+        not(def_class(classe, [notValidClass])),
+	def_class(student, [person],
+		  [field(university, "", string),
+		   method(talk, [],
+			  (writeln("Hi I'm a student!")))
+		  ]),
+	talk(tommi, "I am a monkey"),
+	make(frank, student, [name = "Frank", age = 24]),
+	talk(frank),
+        talk(frank, "I'm also a monkey"),
+        %% il parametro age dovrebbe essere sovrascritto
+	def_class(studentAge, [person],
+		  [field(age, "0", string),
+		   method(talk, [],
+			  (writeln("Hi I'm a student!")))
+		  ]),
+        make(s1, studenteAge, [age = "22"])
     ],
     clear,
     test(Tests).
