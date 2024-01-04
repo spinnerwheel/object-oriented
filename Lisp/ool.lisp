eefe1eecb8ba22e6ef27759dc4488ee27d9cf5aa
davidenicoletta <davidenicoletta@penguin>
2024-01-04 12:29:09 +0100

    Inizio lisp

---
+++ Lisp/ool.lisp
@@ -0,0 +1,20 @@
+;;;; -*- Mode: Lisp -*-
+
+
+;;;; def-class/3
+(defun def-class(class-name parents part)
+  (setf class-name (list parents part)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+;;; end of file -- ool.lisp
